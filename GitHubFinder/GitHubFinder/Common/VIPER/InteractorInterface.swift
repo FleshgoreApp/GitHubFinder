67d476a9662ca72c60d05d8e20d4f634db6c6495
Anton Shvets <fleshgore9000@gmail.com>
2020-12-07 19:28:36 +0200

    Modules + VIPER

---
+++ GitHubFinder/GitHubFinder/Common/VIPER/InteractorInterface.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  GitHubFinder
+//
+//  Created by Saul Goodman on 07.12.2020.
+//
+
+protocol InteractorInterface: class {
+}
+
+extension InteractorInterface {
+}
---
+++ GitHubFinder/GitHubFinder/Common/VIPER/PresenterInterface.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  GitHubFinder
+//
+//  Created by Saul Goodman on 07.12.2020.
+//
+
+protocol PresenterInterface: class {
+}
+
+extension PresenterInterface {
+}
